6c930b638d802ca356803159e4b28c1e0f28eea2
Neeqstock <nicola.davanzo01@gmail.com>
2021-01-26 16:50:43 +0100

    Cose nuove

--- Surface/HTFeedbackModule.cs
+++ Surface/HTFeedbackModule.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NeeqDMIs.Headtracking.NeeqHT;
+using Netytar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,17 +17,46 @@ namespace Eyerpheus.Controllers.Graphics
         public Canvas Canvas;
         public double CenterX;
         public double CenterY;
-         
+        public Line horLine;
+        public Line verLine;
 
-        public HTFeedbackModule(Canvas canvas)
+        private const int horLineThickness = 4;
+        private const int verLineThickness = 4;
+        private readonly SolidColorBrush horLineStroke;
+        private readonly SolidColorBrush verLineStroke;
+
+        public HTFeedbackModes Mode { get; set; }
+
+        public HTFeedbackModule(Canvas canvas, HTFeedbackModes mode)
         {
-            this.Canvas = canvas;
+            Canvas = canvas;
+            
+            horLine = new Line();
+            horLine.StrokeThickness = horLineThickness;
+            horLine.Stroke = horLineStroke;
+            canvas.Children.Add(horLine);
+
+            verLine = new Line();
+            verLine.StrokeThickness = verLineThickness;
+            verLine.Stroke = verLineStroke;
+            canvas.Children.Add(verLine);
+            
+            Mode = mode;
         }
 
-        /*public void UpdateGraphics()
+        public void UpdateGraphics(HeadTrackerData headTrackerData, NetychordsButton lastCheckedButton)
         {
+            horLine.X1 = Canvas.GetLeft(lastCheckedButton) + lastCheckedButton.ActualWidth / 2;
+            horLine.Y1 = Canvas.GetTop(lastCheckedButton) + lastCheckedButton.ActualHeight / 2;
             
-        }*/
+            horLine.X2 = Canvas.
+        }
 
+        public enum HTFeedbackModes
+        {
+            Bars,
+            HalfButton,
+            DeadZone
+        }
     }
 }
